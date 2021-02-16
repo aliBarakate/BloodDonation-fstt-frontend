c2964888e7bc79ff1b81d313ab821c941f7819ff
BARAKATE Ali <alibarakatew@gmail.com>
2021-02-16 14:29:39 +0000

    can send email to selected element list of donators

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,15 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
   responses : any;
+  http2 :any;
   //url:any;
   texturl='http://localhost:8080/findAllDonneursForReceveur';
   constructor(http: HttpClient) {
+    this.http2=http
     //this.responses = new Array<any>();
     http.get('http://localhost:8080/findAllDonneursForReceveur'+'/?ville=RABAT'+'&grpSanguin=AB_pos')
     .subscribe(responses => {
         console.log(responses);
         this.responses=responses;
     });
+  }
+
+  onClickElement(mail) {
+        //this.responses = new Array<any>();
+        this.http2.get('http://localhost:8080/sendmail?mail='+mail)
+        .subscribe(responses => {
+            console.log(responses);
+            this.responses=responses;
+        });
+    console.log(mail)
   }
 
   ngOnInit(): void {
