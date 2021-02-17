4639a469f3f09415a399e66d9e55ad6d57baad2d
BARAKATE Ali <alibarakatew@gmail.com>
2021-02-17 00:54:21 +0000

    toast ajoute et fonctionne correctement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
+import {ToastModule} from 'primeng/toast';
+
+
 import {ButtonModule} from 'primeng/button';
 import {DropdownModule} from 'primeng/dropdown';
 import { MedecinComponent } from './medecin/medecin.component';
@@ -42,7 +45,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     ButtonModule,
     DropdownModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ToastModule
 
 
   ],
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,18 +1,20 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {MessageService} from 'primeng/api';
 
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
+  providers: [MessageService]
 })
 export class PostsComponent implements OnInit {
   responses : any;
   http2 :any;
   //url:any;
   texturl='http://localhost:8080/findAllDonneursForReceveur';
-  constructor(http: HttpClient) {
+  constructor(http: HttpClient,private messageService: MessageService) {
     this.http2=http
     //this.responses = new Array<any>();
     http.get('http://localhost:8080/findAllDonneursForReceveur'+'/?ville=RABAT'+'&grpSanguin=AB_pos')
@@ -30,7 +32,12 @@ export class PostsComponent implements OnInit {
             this.responses=responses;
         });
     console.log(mail)
+    this.showSuccess();
   }
+
+  showSuccess() {
+    this.messageService.add({severity:'success', summary: 'Email envoyé avec Success', detail: 'Le donneur de sang à était notifié'});
+}
 
   ngOnInit(): void {
   }
